ae18da7258bb6369958b6a77b57576b41f587436
Javohir Toshkanov <toshkanovjavohir200@gmail.com>
2026-05-01 14:12:55 +0500

    file name edi

--- uptimer.py
+++ uptimer.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
-
+import os
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -21,3 +21,8 @@ def start_uptimer():
     # Asosiy bot ishlashiga xalaqit bermasligi uchun alohida oqimda ishga tushiramiz
     thread = threading.Thread(target=run_uptimer, daemon=True)
     thread.start()
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000)) # Render bergan portni oladi
+    app.run(host="0.0.0.0", port=port) # 0.0.0.0 barcha manzillardan eshitadi
